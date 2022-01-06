32f236efc46a8b176ca0b3f768b7485aeae11d71
Daniela Blase <daniela.blase@yahoo.de>
2022-01-06 15:53:31 +0100

    installed styled-components, react router, implemented google fonts

---
+++ GlobalStyles.jsx
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+:root {
+  --main-background: ;
+  --color-one: ;
+  --color-two: ;
+  --color-two: ;
+}
+body {
+  margin: 0;
+  padding: 0;
+  background: var(--background-one);
+  font-family: 'Montserrat', sans-serif;
+}
+
+h1 {
+margin: 0;
+}
+
+`;
+
+export default GlobalStyle;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter } from "reactö-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
