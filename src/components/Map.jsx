057813b399d882249d55705d33e91add72f96044
Daniela Blase <daniela.blase@yahoo.de>
2022-01-25 20:32:19 +0100

    tried map rendering

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,27 @@
 import styled from "styled-components";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Map() {
   return (
     <>
-      <MapContainer id="map"></MapContainer>
+      <MapContainer
+        style={{ height: "20px" }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </>
   );
 }
 
 export default Map;
-
-const MapContainer = styled.div`
-  height: 20px;
-`;
