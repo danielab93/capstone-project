e5554061ca61d305fa34d7062543966913328132
Daniela Blase <daniela.blase@yahoo.de>
2022-01-25 17:25:04 +0100

    created map component

---
+++ src/components/Map.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function Map() {
+  return (
+    <>
+      <MapContainer id="map"></MapContainer>
+    </>
+  );
+}
+
+export default Map;
+
+const MapContainer = styled.div`
+  height: 20px;
+`;
