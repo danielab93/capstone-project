43cd856e4f551da8eb442daf51fa797a9e24584b
Daniela Blase <daniela.blase@yahoo.de>
2022-01-27 16:42:46 +0100

    calculates now with 3 inputs

--- src/components/Co2Calculator.jsx
+++ src/components/Co2Calculator.jsx
@@ -42,68 +42,52 @@ function Co2Calculator() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (values.inputCar) {
+    if (
+      values.inputCar > 0 ||
+      values.inputTrain > 0 ||
+      values.inputFlight > 0
+    ) {
       const totalCarKm = co2Calculation.carKm + values.inputCar;
       const co2Car = (co2Calculation.carKm + values.inputCar) * 0.119;
       const co2CarRounded = Math.round(co2Car * 100) / 100;
+
+      const totalCo2Car = Math.round(values.inputCar * 0.119 * 100) / 100;
+
+      // TRAIN
+      const totalTrainKm = co2Calculation.trainKm + values.inputTrain;
+      const co2Train = (co2Calculation.trainKm + values.inputTrain) * 0.032;
+      const co2TrainRounded = Math.round(co2Train * 100) / 100;
+
+      const totalCo2Train = Math.round(values.inputTrain * 0.032 * 100) / 100;
+
+      // FLIGHT
+      const totalFlightKm = co2Calculation.flightKm + values.inputFlight;
+      const co2Flight = (co2Calculation.flightKm + values.inputFlight) * 0.38;
+      const co2FlightRounded = Math.round(co2Flight * 100) / 100;
+
+      const totalCo2Flight = Math.round(values.inputFlight * 0.38 * 100) / 100;
+
       const co2BudgetSpent =
-        Math.round(
-          (co2Calculation.co2BudgetSpent + values.inputCar * 0.119) * 100
-        ) / 100;
+        co2Calculation.co2BudgetSpent +
+        Math.round((totalCo2Car + totalCo2Train + totalCo2Flight) * 100) / 100;
+
       const co2BudgetLeft =
-        Math.round(
-          (co2Calculation.co2BudgetLeft - values.inputCar * 0.119) * 100
-        ) / 100;
+        co2Calculation.co2BudgetLeft -
+        Math.round((totalCo2Car + totalCo2Train + totalCo2Flight) * 100) / 100;
+
       setCo2Calculation({
         ...co2Calculation,
         carKm: totalCarKm,
         co2Car: co2CarRounded,
-        co2BudgetSpent: co2BudgetSpent,
-        co2BudgetLeft: co2BudgetLeft,
-      });
-      setValues(initialValues);
-      setHasFormErrors(false);
-    } else if (values.inputTrain) {
-      const totalTrainKm = co2Calculation.trainKm + values.inputTrain;
-      const co2Train = (co2Calculation.trainKm + values.inputTrain) * 0.032;
-      const co2TrainRounded = Math.round(co2Train * 100) / 100;
-      const co2BudgetSpent =
-        Math.round(
-          (co2Calculation.co2BudgetSpent + values.inputTrain * 0.032) * 100
-        ) / 100;
-      const co2BudgetLeft =
-        Math.round(
-          (co2Calculation.co2BudgetLeft - values.inputTrain * 0.032) * 100
-        ) / 100;
 
-      setCo2Calculation({
-        ...co2Calculation,
         trainKm: totalTrainKm,
         co2Train: co2TrainRounded,
-        co2BudgetSpent: co2BudgetSpent,
-        co2BudgetLeft: co2BudgetLeft,
-      });
-      setValues(initialValues);
-      setHasFormErrors(false);
-    } else if (values.inputFlight) {
-      const totalFlightKm = co2Calculation.flightKm + values.inputFlight;
-      const co2Flight = (co2Calculation.flightKm + values.inputFlight) * 0.38;
-      const co2FlightRounded = Math.round(co2Flight * 100) / 100;
-      const co2BudgetSpent =
-        Math.round(
-          (co2Calculation.co2BudgetSpent + values.inputFlight * 0.38) * 100
-        ) / 100;
-      const co2BudgetLeft =
-        Math.round(
-          (co2Calculation.co2BudgetLeft - values.inputFlight * 0.38) * 100
-        ) / 100;
 
-      setCo2Calculation({
-        ...co2Calculation,
         flightKm: totalFlightKm,
         co2Flight: co2FlightRounded,
-        co2BudgetSpent: co2BudgetSpent,
-        co2BudgetLeft: co2BudgetLeft,
+
+        co2BudgetSpent: Math.round(co2BudgetSpent * 100) / 100,
+        co2BudgetLeft: Math.round(co2BudgetLeft * 100) / 100,
       });
       setValues(initialValues);
       setHasFormErrors(false);
